e4309c805e7896d70690c5ef626ad8ca5ca69ba3
Edward Koikai <edwardkoikai@gmail.com>
2024-03-22 18:48:20 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+// Task number two
+const mondayWork = function (ToDo = "go to the office") {
+  return `This Monday, I will ${ToDo}.`;
+};
+mondayWork();
+
+// test three
+function wrapAdjective(msg1= "*"){
+    return function(msg2 = "special"){
+        return `You are ${msg1}${msg2}${msg1}!`
+    }
+}
+console.log(wrapAdjective("%")("a dedicated programmer"));
